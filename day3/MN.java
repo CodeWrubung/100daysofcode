8ef3fa76970a514b9d8a316fd4bb909202d1eff5
Rubung <rubungswargiary99@gmail.com>
2025-09-02 10:11:11 +0530

    Update MN.java

--- day3/MN.java
+++ day3/MN.java
@@ -1,1 +1,23 @@
+public class MN {
+    public static void main(String[] args){
+        int age=90;
+        int my ;
+        my=60;
+        System.out.println(my);
+        age=80;
+        System.out.println(age);
+
+        boolean isMyage =true;
+        System.out.println(isMyage);
+
+        double myD= 6.12;
+        System.out.println(myD);
+
+        float myF = 5.0f;
+        System.out.println(myF);
+
+        String wishes = "Good Morning";
+        System.out.println(wishes);
+    }
+}
 
