88893f652ad2ececc01719fc8c3f13d0d70ff2d5
Rubung <rubungswargiary99@gmail.com>
2025-09-02 10:30:21 +0530

    Create New.java

---
+++ day3/New.java
@@ -0,0 +1,12 @@
+public class New{
+    public static void main(String[] args){
+        System.out.println("My name is Rubung");
+        int age=20;
+        System.out.println("I am "+ age+ " years old");
+        double height = 1.75;
+        System.out.println("My height is "+ height +"meters");
+        boolean likeCoding = true;
+        System.out.println("Do I like coding ? " + likeCoding);
+
+    }
+}
